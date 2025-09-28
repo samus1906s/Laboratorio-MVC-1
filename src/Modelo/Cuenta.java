0de06d1155f13e993e788ca11d1214e70f968177
Brandon Valdelomar <brvaldelomarco@est.utn.ac.cr>
2025-09-27 20:00:23 -0600

    Modificacion.
    Se modifica clase cuenta y se agrega atributo monto.

--- src/Modelo/Cuenta.java
+++ src/Modelo/Cuenta.java
@@ -11,8 +11,9 @@ import java.util.Objects;
  * @author Valdelomaar
  */
 public class Cuenta {
-    private final String numero;
+     private final String numero;
     private final String titularId;
+    private double monto;
     private double saldo;
     private EstadoCuenta estado;
     private final TipoCuenta tipo;
@@ -21,16 +22,44 @@ public class Cuenta {
         this.numero = Objects.requireNonNull(numero);
         this.titularId = Objects.requireNonNull(titularId);
         this.tipo = Objects.requireNonNull(tipo);
+        this.monto = 0.0;
         this.saldo = 0.0;
         this.estado = EstadoCuenta.ACTIVA;
     }
 
-    public String getNumero() { return numero; }
-    public String getTitularId() { return titularId; }
-    public double getSaldo() { return saldo; }
-    public EstadoCuenta getEstado() { return estado; }
-    public TipoCuenta getTipo() { return tipo; }
+    public String getNumero() {
+        return numero;
+    }
 
-    public void setSaldo(double saldo) { this.saldo = saldo; }
-    public void setEstado(EstadoCuenta estado) { this.estado = estado; }
+    public String getTitularId() {
+        return titularId;
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public EstadoCuenta getEstado() {
+        return estado;
+    }
+
+    public void setEstado(EstadoCuenta estado) {
+        this.estado = estado;
+    }
+
+    public TipoCuenta getTipo() {
+        return tipo;
+    }
 }
